d92f847ff8fc2fdb2a3f845112e3a0a08f0b0d84
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-08-25 13:29:55 +0700

    Created model form to add models. Implemented custom validators for the model input.

--- controllers/formController.js
+++ controllers/formController.js
@@ -2,6 +2,7 @@ const db = require("../db/queries");
 const { validationResult } = require("express-validator");
 const validatePartEditForm = require("./validators/editItemValidator");
 const validateBrand = require("./validators/brandValidator");
+const validateModel = require("./validators/modelValidator");
 
 const {
   getBrandCategories,
@@ -63,6 +64,37 @@ exports.getBrandForm = async (req, res) => {
 
 exports.postBrandCategory = [
   validateBrand,
+  async (req, res) => {
+    let brand_categories = await getBrandCategories();
+    let vehicleType_categories = await getVehicleTypesCategories();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", {
+        part_id: req.params.part_id,
+        errors: errors.array(),
+        title: `Error editing Item: ${req.body.part_id}`,
+        brand_categories,
+        vehicleType_categories,
+      });
+    }
+    res.send("TO BE IMPLEMENTED");
+  },
+];
+
+exports.getModelForm = async (req, res) => {
+  let brand_categories = await getBrandCategories();
+  let vehicleType_categories = await getVehicleTypesCategories();
+
+  res.render("form", {
+    title: `Add Brand Category`,
+    brand_categories,
+    vehicleType_categories,
+    modelForm: true,
+  });
+};
+
+exports.postModel = [
+  validateModel,
   async (req, res) => {
     let brand_categories = await getBrandCategories();
     let vehicleType_categories = await getVehicleTypesCategories();
--- routes/formRouter.js
+++ routes/formRouter.js
@@ -8,4 +8,7 @@ formRouter.post("/edit/part/:part_id", formController.insertUpdatedItem);
 formRouter.get("/add/car_brands", formController.getBrandForm);
 formRouter.post("/add/car_brands", formController.postBrandCategory);
 
+formRouter.get("/add/car_models", formController.getModelForm);
+formRouter.post("/add/car_models", formController.postModel);
+
 module.exports = formRouter;
